88b1cf9359d0f189827ff49ab3b8bcbd49059873
Asion001 <asion.desu@gmail.com>
2025-03-08 20:31:16 +0200

    init

---
+++ lib/jaspr_options.dart
@@ -0,0 +1,31 @@
+// GENERATED FILE, DO NOT MODIFY
+// Generated with jaspr_builder
+
+import 'package:jaspr/jaspr.dart';
+import 'package:asion_dev/pages/home.dart' as prefix0;
+import 'package:asion_dev/app.dart' as prefix1;
+
+/// Default [JasprOptions] for use with your jaspr project.
+///
+/// Use this to initialize jaspr **before** calling [runApp].
+///
+/// Example:
+/// ```dart
+/// import 'jaspr_options.dart';
+///
+/// void main() {
+///   Jaspr.initializeApp(
+///     options: defaultJasprOptions,
+///   );
+///
+///   runApp(...);
+/// }
+/// ```
+final defaultJasprOptions = JasprOptions(
+  clients: {
+    prefix0.Home: ClientTarget<prefix0.Home>('pages/home'),
+  },
+  styles: () => [
+    ...prefix1.App.styles,
+  ],
+);
